528ef72bfae1315a27a93d9eb3563a7d76cfe405
Skadusch <Skadusch@proton.me>
2024-05-18 15:31:27 +0200

    Revert "gitignore"
    This reverts commit 15b8ab951ba835780dd333015a1daf03da0eff7a.

--- lua/config/lazy/local.lua
+++ lua/config/lazy/local.lua
@@ -1,7 +1,4 @@
 return {
-	{ "Tets", dir = "~\\projects\\plugins\\calc.nvim\\" },
-	{
-		"stackmap",
-		dir = "~\\projects\\plugins\\stackmap.nvim\\",
-	},
+	"Tets",
+	dir = "~\\projects\\plugins\\calc.nvim\\",
 }
