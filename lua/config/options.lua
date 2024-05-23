b1ec24931c61186d8969259d5695372b6da6acda
Skadusch <Skadusch@proton.me>
2024-05-23 21:05:29 +0200

    readded tapstob :)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,6 +15,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.opt.guicursor = "a:block"
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -29,7 +34,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = ".vim/undodir"
+vim.opt.undodir = ".undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
